a20eac4d0a5efa17bb10ba5f6ecb5bd8405cde49
smdurjoy <smdurjoy963@gmail.com>
2023-06-11 20:19:47 +0600

    job description page done

--- src/components/Jobs/JobDescription.js
+++ src/components/Jobs/JobDescription.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../assets/css/jobDescription.css';
+import companyLogo from '../../images/companies/company1.jpg';
 import m1 from '../../images/companies/jobs/m1.jpg';
 import m2 from '../../images/companies/jobs/m2.webp';
 import m3 from '../../images/companies/jobs/m3.jpg';
@@ -208,6 +209,90 @@ const JobDescription = () => {
                                     <p>Application ends: <span>October 1 2025</span></p>
                                     <button className="applyBtn">Apply Now</button>
                                     <button className="shortlistBtn">Shortlist</button>
+                                    <p className="text-center m-lg-auto py-3">OR APPLY WITH</p>
+                                    <div className="applyWith">
+                                        <button className="btn facebook">
+                                            <i className="fa fa-facebook"></i>&nbsp;
+                                            facebook
+                                        </button>
+                                        <button className="btn google">
+                                            <i className="fa fa-google"></i>&nbsp;
+                                            google
+                                        </button>
+                                        <button className="btn linkedin">
+                                            <i className="fa fa-linkedin"></i>&nbsp;
+                                            linkedin
+                                        </button>
+                                        <button className="btn twitter">
+                                            <i className="fa fa-twitter"></i>&nbsp;
+                                            twitter
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card py-3 px-2 mt-4">
+                            <div className="card-body candidateAlert companyInfoArea">
+                                <div className="text-center headerArea">
+                                    <img src={companyLogo} alt="company"/>
+                                    <p>Skype</p>
+                                </div>
+                                <div className="content d-flex align-items-center">
+                                    <i className="fa fa-phone"></i>&nbsp;&nbsp;
+                                    <div>
+                                        <span className="text-dark">Phone</span> <br/>
+                                        <span>+084 123 456 789</span>
+                                    </div>
+                                </div>
+                                <div className="content d-flex align-items-center mt-3">
+                                    <i className="fa fa-envelope-o"></i>&nbsp;
+                                    <div>
+                                        <span className="text-dark">Email</span> <br/>
+                                        <span>+gaddis@apus.com</span>
+                                    </div>
+                                </div>
+                                <div className="content d-flex align-items-center mt-3">
+                                    <i className="fa fa-rss"></i>&nbsp;&nbsp;
+                                    <div>
+                                        <span className="text-dark">Website</span> <br/>
+                                        <span>apusthemes.com</span>
+                                    </div>
+                                </div>
+                                <div className="content d-flex align-items-center mt-3">
+                                    <i className="fa fa-map-marker"></i>&nbsp;&nbsp;&nbsp;&nbsp;
+                                    <div>
+                                        <span className="text-dark">Location</span> <br/>
+                                        <span>Washington D.C, United States</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card py-3 px-2 mt-4">
+                            <div className="card-body candidateAlert companyInfoArea">
+                                <h5>Job Location</h5>
+                                {/* eslint-disable-next-line jsx-a11y/iframe-has-title */}
+                                <iframe
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2990.274257380938!2d-70.56068388481569!3d41.45496659976631!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e52963ac45bbcb%3A0xf05e8d125e82af10!2sDos%20Mas!5e0!3m2!1sen!2sus!4v1671220374408!5m2!1sen!2sus"
+                                    width="100%" height="300" style={{border: 0, marginTop: '15px'}} allowFullScreen=""
+                                    loading="lazy"
+                                    referrerPolicy="no-referrer-when-downgrade"></iframe>
+                                <div className="content d-flex align-items-center mt-4">
+                                    <i className="fa fa-compass"></i>&nbsp;&nbsp;
+                                    <span>
+                                        3 <span className="text-dark">Years</span> Ago
+                                    </span>
+                                </div>
+                                <div className="content d-flex align-items-center mt-4">
+                                    <i className="fa fa-eye"></i>&nbsp;&nbsp;
+                                    <span>
+                                        <span className="text-dark">48</span> Views
+                                    </span>
+                                </div>
+                                <div className="content d-flex align-items-center mt-4">
+                                    &nbsp;&nbsp;<i className="fa fa-mobile"></i>&nbsp;&nbsp;&nbsp;&nbsp;
+                                    <span>
+                                        <span className="text-dark">1</span> Applicant
+                                    </span>
                                 </div>
                             </div>
                         </div>
