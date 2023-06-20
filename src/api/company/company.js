6377bbd7fba24e6f0b90172a8b7baa7385bad6ef
smdurjoy <smdurjoy963@gmail.com>
2023-06-21 00:48:54 +0600

    company profile page dynamic

---
+++ src/api/company/company.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+import {toastError} from "../../Helpers/Toaster";
+
+export const fetchCompanyProfileInfo = async (companyId) => {
+    try {
+        const {data: {data}} = await axios.get(`/company/details/${companyId}`);
+        return data;
+    } catch (e) {
+        toastError('Something Went Wrong!');
+    }
+}
