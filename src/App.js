278300feed0620803a3b2a431d1aaf7919a4d573
smdurjoy <smdurjoy963@gmail.com>
2023-08-09 21:00:33 +0600

    version2 initialize

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
-import 'font-awesome/css/font-awesome.min.css';
-import './assets/css/responsive.css';
-import 'react-toastify/dist/ReactToastify.min.css';
-import {RouterProvider} from "react-router-dom";
-import AppRoute from "./router/AppRoute";
-import {ToastContainer} from "react-toastify";
-import React from "react";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <>
-            <ToastContainer/>
-            <RouterProvider router={AppRoute}/>
-        </>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
