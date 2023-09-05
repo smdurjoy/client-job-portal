35a9664883984210e29cd1718be7b9eb38e45b69
unknown <oahidzihad1@gmail.com>
2023-09-05 21:37:07 +0600

    add baseUrl from proxy

--- src/services/country.js
+++ src/services/country.js
@@ -1,20 +1,24 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://www.kamla.xyz' : '';
+
+
 export const countryApi = createApi({
     reducerPath: 'countryApi',
     tagTypes: ['countries'],
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://kamla.xyz/',
+        baseUrl: baseUrl,
         prepareHeaders: (headers) => {
-            headers.set("Content-type", "application/json");
             headers.set('Access-Control-Allow-Origin', '*');
+            headers.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+            headers.set("Content-type", "application/json");
             return headers;
         }
     }),
     endpoints: (builder) => ({
         getCountries: builder.query({
             query: () => `common/countries/`,
-            providesTags: 'countries'
+            // providesTags: 'countries'
         }),
     }),
 })
